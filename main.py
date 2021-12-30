77b77d830386c353d8fd26c1e49e2d6461a5fc16
nepp-plus <nepp.plus@gmail.com>
2021-12-30 10:35:57 +0900

    코드 구조 변경
     - DB 연결 관련 : phone_book.py 모듈로 이동
     - 메인 : 메뉴 표시 기능 실행

--- main.py
+++ main.py
@@ -2,22 +2,7 @@
 # 단순 동작 위주의 코드 작성
 # DB 연결 => 기타기능 : phone_book.py 모듈 이용.
 
-import pymysql
-from phone_book import set_db_info
+from phone_book import print_main_menu
 
-# DB에 연결을 해야 -> 쿼리를 날릴 수 있다.
+menu_num = print_main_menu()
 
-# DB 연결부터 진행
-# DB 연결시도의 결과를 변수에 담자.
-
-# 접속 정보만 명시
-db = pymysql.connect(
-    host='finalproject.cbqjwimiu76h.ap-northeast-2.rds.amazonaws.com', 
-    port=3306, 
-    user='admin',
-    passwd='Vmfhwprxm!123',
-    db='test_phone_book',
-    charset='utf8')
-
-# phone_book.py에서 로직 작성 => 연결정보를 넘겨주고, 그 파일에서 활용.
-set_db_info(db)
--- phone_book.py
+++ phone_book.py
@@ -1,12 +1,25 @@
+import pymysql
 # DB 연결자체는 main.py에서 설정
 # 만들어진 연결정보를 받아서 사용해보자.
 
-db_connect = None
+db_connect = pymysql.connect(
+    host='finalproject.cbqjwimiu76h.ap-northeast-2.rds.amazonaws.com', 
+    port=3306, 
+    user='admin',
+    passwd='Vmfhwprxm!123',
+    db='test_phone_book',
+    charset='utf8')
+
 
 # 쿼리를 날려주는 역할
-cursor = None
+cursor = db_connect.cursor()
 
-def set_db_info(connect):
-    # db_connect라는 비어있는 변수에 연결정보 대입.
-    db_connect = connect
-    cursor = db_connect.cursor()
+
+def print_main_menu():
+    print('====== 전화번호부 ======')
+    print('1. 로그인')
+    print('2. 회원가입')
+    print('0. 프로그램 종료')
+    print('=======================')
+    menu_num =  int( input('메뉴 선택 : ') )
+    return menu_num
