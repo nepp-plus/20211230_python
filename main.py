809782dac00eeea7e936aa0e58b68a90fd162a48
nepp-plus <nepp.plus@gmail.com>
2021-12-30 11:37:24 +0900

    로그인 후의 메인 메뉴 기능
     - 로그아웃할때까지 무한반복

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 # 단순 동작 위주의 코드 작성
 # DB 연결 => 기타기능 : phone_book.py 모듈 이용.
 
-from phone_book import print_main_menu, sign_up, sign_in
+from time import sleep
+from phone_book import print_main_menu, sign_up, sign_in, print_phone_book_menu
 
 
 # 프로그램이 종료될 사유가 생길때까지 무한반복.
@@ -13,7 +14,17 @@ while True:
     if menu_num == 2:
         sign_up()
     elif menu_num == 1:
-        sign_in()
+        login_result = sign_in()
+        
+        # 로그인에 성공했다면? 개인별 연락처 메뉴로 이동.
+        if login_result:
+            while True:
+                num = print_phone_book_menu()
+                if num == 0:
+                    print('로그아웃 후 메인으로 돌아갑니다.')
+                    sleep(2)
+                    break
+                
     elif menu_num == 0:
         print('프로그램을 종료합니다.')
         break
